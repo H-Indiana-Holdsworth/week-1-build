28df11479b2bf65b1ead22c3c8be3cb849a55320
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2021-10-08 13:29:48 -0700

    Adds working random number generator button

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 // import functions and grab DOM elements
+const randomNumP = document.getElementById('random-num');
+const randomNum = Math.floor(Math.random() * 10);
 
+const button = document.getElementById('generate');
 // initialize global state
 
 // set event listeners 
+button.addEventListener('click', ()=> {
+    randomNumP.textContent = randomNum;
+});
+
+console.log(randomNum);
 // get user input
 // use user input to update state 
 // update DOM to reflect the new state
